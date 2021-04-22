8a5a6a8bb5a7e2ace46923ab77b650b949576c81
Ronny <ron.cg95@gmail.com>
2021-04-21 20:25:35 -0500

    Initial Commit

--- Main.cs
+++ Main.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Wox.Plugin.Translator
 {
-    public class Main
+    class Main : IPlugin
     {
+        public List<Result> Query(Query query)
+        {
+            var result = new Result
+            {
+                Title = "Hello World from Auto-Translator!",
+                SubTitle = $"Query: {query.Search}",
+                IcoPath = Path.Combine("Images", "translate.png")
+            };
+            return new List<Result> { result };
+        }
+
+        public void Init(PluginInitContext context)
+        {
+
+        }
     }
 }
